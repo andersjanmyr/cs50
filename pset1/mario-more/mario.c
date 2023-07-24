dc1ee868094768667f0bb89764e7b10d31b94f0f
Anders Janmyr <anders@janmyr.com>
2023-07-24 12:58:03 +0200

    Solved mario

--- pset1/mario-more/mario.c
+++ pset1/mario-more/mario.c
@@ -1,7 +1,26 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void) 
+int read_height() {
+    return get_int("Height: ");
+}
+
+void bricks(int c) {
+
+}
+
+void print_pyramid(int height)
 {
-    
+    for (int i = 1; i <= height; i++) {
+        printf(" %.*s%.*s %.*s\n", height-i, "        ", i, "########", i, "########");
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    int height = 0;
+    if (argc > 1) height = atoi(argv[1]);
+    while (height < 1 || height > 8) height = read_height();
+    print_pyramid(height);
 }
