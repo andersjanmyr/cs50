a84c5afbd6588ecf78c8ee30c1b6f889a509eb86
Anders Janmyr <anders@janmyr.com>
2023-07-27 12:35:23 +0200

    Solved scrabble

--- pset2/scrabble/scrabble.c
+++ pset2/scrabble/scrabble.c
@@ -1,15 +1,28 @@
 #include <ctype.h>
 #include <cs50.h>
+#include <math.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
 
+const int values[] = { 1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10 };
 
-bool is_alpha(string s) {
-    if (strlen(s) == 0) return false;
+bool is_empty(string s) {
+    return strlen(s) == 0;
+}
+
+int value_char(char c) {
+    if (isalpha(c)) {
+        return values[toupper(c) - 'A'];
+    }
+    return 0;
+}
+
+int value(string s) {
+    int v = 0;
     for (int i = 0, l = strlen(s); i < l; i++)
-       if(!isalpha(s[i])) return false;
-    return true;
+       v += value_char(s[i]);
+    return v;
 }
 
 int main(int argc, char *argv[])
@@ -20,10 +33,18 @@ int main(int argc, char *argv[])
         word1 = argv[1];
         word2 = argv[2];
     }
-    while (!is_alpha(word1) && !is_alpha(word2)) {
+    while (is_empty(word1) || is_empty(word2)) {
         word1 = get_string("Player 1: ");
         word2 = get_string("Player 2: ");
     }
-    printf("%s:%s\n", word1, word2);
+    int v1 = value(word1);
+    int v2 = value(word2);
+    if (v1 == v2)
+        printf("Tie!\n");
+    else if (v1 > v2)
+        printf("Player 1 wins!\n");
+    else
+        printf("Player 2 wins!\n");
+
 }
 
